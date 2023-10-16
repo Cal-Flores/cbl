3ff9a594f09f2fd2ec052046656246ea2bc25b60
calebflores <calebflores1388@gmail.com>
2023-10-16 13:41:04 -0500

    get one fighter

--- app/api/fighter_routes.py
+++ app/api/fighter_routes.py
@@ -7,16 +7,22 @@ fighter_routes = Blueprint('fighters', __name__)
 
 
 ########### Get Fighters ############
-@fighter_routes.route('/all_fighters/<int:weight>')
+@fighter_routes.route('/<int:weight>')
 def get_weight_fighters(weight):
     fighters = Fighter.query.filter(Fighter.weight == weight)
     all_fighters = []
     all_fighters.extend([i.to_dict() for i in fighters])
     return {'All_Fighters': all_fighters}
 
-@fighter_routes.route('/all_fighters')
+@fighter_routes.route('/')
 def get_all_fighters():
     fighters = Fighter.query.all()
     all_fighters = []
     all_fighters.extend([i.to_dict() for i in fighters])
     return {'All_Fighters': all_fighters}
+
+@fighter_routes.route('/fighter/<int:id>')
+def get_one_fighter(id):
+    fighter = Fighter.query.get(id)
+    fight = fighter.to_dict()
+    return {'Fighter': fight}
