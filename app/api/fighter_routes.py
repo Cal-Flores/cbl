f57ece41d7a56becfa3eb9495c8caa6ca4591cd0
calebflores <calebflores1388@gmail.com>
2023-10-16 13:30:40 -0500

    get fighters by weight

--- app/api/fighter_routes.py
+++ app/api/fighter_routes.py
@@ -3,10 +3,17 @@ from flask_login import login_required
 from app.models import User, Fighter
 
 fighter_routes = Blueprint('fighters', __name__)
-#url_prefix='/api/fighters'
+#url_prefix='/api/fighter'
 
 
 ########### Get Fighters ############
+@fighter_routes.route('/all_fighters/<int:weight>')
+def get_weight_fighters(weight):
+    fighters = Fighter.query.filter(Fighter.weight == weight)
+    all_fighters = []
+    all_fighters.extend([i.to_dict() for i in fighters])
+    return {'All_Fighters': all_fighters}
+
 @fighter_routes.route('/all_fighters')
 def get_all_fighters():
     fighters = Fighter.query.all()
