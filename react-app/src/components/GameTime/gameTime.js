fde9d1b40e5929fe78dbe784142e8df95e18c400
calebflores <calebflores1388@gmail.com>
2023-10-30 17:28:55 -0500

    game time set up ready for form

--- react-app/src/components/GameTime/gameTime.js
+++ react-app/src/components/GameTime/gameTime.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import './gameTime.css';
 
 function GameTime() {
     const dispatch = useDispatch();
@@ -25,11 +26,62 @@ function GameTime() {
     }, [id]);
 
     console.log('Dual Info:', dualInfo);
+    let squadOne;
+    let squadTwo;
+    if (dualInfo?.team_1) {
+        squadOne = Object.values(dualInfo?.team_1).sort((a, b) => a.weight - b.weight)
+        console.log('HERE IS SQUAD 1', squadOne)
+    }
+    if (dualInfo?.team_2) {
+        squadTwo = Object.values(dualInfo?.team_2).sort((a, b) => a.weight - b.weight)
+        console.log('HERE IS SQUAD 2', squadTwo)
+    }
 
+    let scoreOne = 0;
+    let scoreTwo = 0;
     return (
-        <div style={{ marginTop: '6%' }}>
-            <h1>GAME DAY</h1>
-            {/* Render the retrieved data in your component */}
+        <div id='gameday'>
+            <h1 style={{ textAlign: 'center' }}>Week {dualInfo?.dual?.week} Dual  #{dualInfo?.dual?.id}</h1>
+            <div className='gamehead'>
+                <div className='scoreteam'>
+                    <div>{dualInfo?.dual?.team_1}</div>
+                    <div>{scoreOne}</div>
+                </div>
+                <div className='gamevs'>vs</div>
+                <div className='scoreteam'>
+                    <div>{scoreTwo}</div>
+                    <div>{dualInfo?.dual?.team_2}</div>
+                </div>
+            </div>
+            <div id='squadcont'>
+                <div className='squadOne'>
+                    {squadOne?.map(fighter => (
+                        <div>
+                            <img style={{ height: '115px', width: '184px', borderBottom: '1px solid gray', }} onError={(e) => { e.target.src = '../../../images/blank.png' }} src={fighter.prof_img} />
+                            <div>{fighter?.name}</div>
+                            <div>{fighter.points} pts</div>
+                            <div>{fighter?.wins} - {fighter?.losses}</div>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    <form>
+                        
+                    </form>
+                </div>
+
+
+                <div className='squadOne'>
+                    {squadTwo?.map(fighter => (
+                        <div>
+                            <img style={{ height: '115px', width: '184px', borderBottom: '1px solid gray', }} onError={(e) => { e.target.src = '../../../images/blank.png' }} src={fighter.prof_img} />
+                            <div>{fighter?.name}</div>
+                            <div>{fighter.points} pts</div>
+                            <div>{fighter?.wins} - {fighter?.losses}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
