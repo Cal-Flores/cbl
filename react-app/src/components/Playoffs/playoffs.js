9f7a50a739dce54f6595eef73d12456e866a994a
calebflores <calebflores1388@gmail.com>
2023-11-03 18:48:47 -0500

    playoff picture page styled

--- react-app/src/components/Playoffs/playoffs.js
+++ react-app/src/components/Playoffs/playoffs.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
+import './playoffs.css';
 
 function Playoffs() {
     const [playoffData, setPlayoffData] = useState(null);
@@ -24,18 +25,102 @@ function Playoffs() {
 
     return (
         <div style={{ marginTop: '100px' }}>
-            <h1>PLAYOFF PICTURE</h1>
+            <div style={{ fontSize: '22px', marginLeft: '5%' }}>PLAYOFF PICTURE</div>
+            <hr></hr>
             {playoffData &&
                 <div>
-                    <div>
-                        {playoffData?.afc_leaders?.map(team => (
-                            <div>
-                                <div>{team.name}</div>
-                                <img style={{ height: '55px', width: '55px' }} src={team?.logo_img} />
-                            </div>
-                        ))}
+                    <div className='playdiv'>
+                        <div className='playdivson'>
+                            <h3 className='playhead'>Division Leaders</h3>
+                            <p className='playpara'>If the season ended today, these would be the leaders</p>
+                            {playoffData?.afc_leaders?.map((team, index) => (
+                                <div className='playcardwrap'>
+                                    <div className='playcard' key={index}>
+                                        <span>{index + 1}</span>
+                                        <img style={{ height: '55px', width: '55px' }} src={team?.logo_img} />
+                                        <img style={{ width: '150px', height: '28px', marginRight: '15%', marginLeft: '4%' }} src={team.heavy} />
+                                        <div>{team?.curr_wins} - {team?.curr_loss}</div>
+                                        <div>·</div>
+                                        <div>{team?.conf} {team?.divison}</div>
+                                    </div>
+                                    <div className='playbye'>
+                                        <div className='playnext'>Next Week Matchup</div>
+                                        <img style={{ height: '30px', width: '30px' }} src={team?.logo_img} />
+                                        <div>Bye</div>
+                                    </div>
+                                </div>
+
+                            ))}
+                            {playoffData?.afc_playoff_teams?.map((team, index) => (
+                                <div className='playcardwrap'>
+                                    <div className='playcard' key={index}>
+                                        <span>{index + 5}</span>
+                                        <img style={{ height: '55px', width: '55px' }} src={team?.logo_img} />
+                                        <img style={{ width: '150px', height: '28px', marginRight: '15%', marginLeft: '4%' }} src={team.heavy} />
+                                        <div>{team?.curr_wins} - {team?.curr_loss}</div>
+                                        <div>·</div>
+                                        <div>{team?.conf} {team?.divison}</div>
+                                    </div>
+                                    <div className='playbye'>
+                                        <div className='playnext'>Next Week Matchup</div>
+                                        <img style={{ height: '30px', width: '30px' }} src={team?.logo_img} />
+                                        <div>Bye</div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className='playdivson'>
+                            <h3 className='playhead'>Division Leaders</h3>
+                            <p className='playpara'>If the season ended today, these would be the leaders</p>
+                            {playoffData?.nfc_leaders?.map((team, index) => (
+                                <div className='playcardwrap'>
+                                    <div className='playcard' key={index}>
+                                        <span>{index + 1}</span>
+                                        <img style={{ height: '55px', width: '55px' }} src={team?.logo_img} />
+                                        <img style={{ width: '150px', height: '28px', marginRight: '15%', marginLeft: '4%' }} src={team.heavy} />
+                                        <div>{team?.curr_wins} - {team?.curr_loss}</div>
+                                        <div>·</div>
+                                        <div>{team?.conf} {team?.divison}</div>
+                                    </div>
+                                    <div className='playbye'>
+                                        <div className='playnext'>Next Week Matchup</div>
+                                        <img style={{ height: '30px', width: '30px' }} src={team?.logo_img} />
+                                        <div>Bye</div>
+                                    </div>
+                                </div>
+
+                            ))}
+                            {playoffData?.nfc_playoff_teams?.map((team, index) => (
+                                <div className='playcardwrap'>
+                                    <div className='playcard' key={index}>
+                                        <span>{index + 5}</span>
+                                        <img style={{ height: '55px', width: '55px' }} src={team?.logo_img} />
+                                        <img style={{ width: '150px', height: '28px', marginRight: '15%', marginLeft: '4%' }} src={team.heavy} />
+                                        <div>{team?.curr_wins} - {team?.curr_loss}</div>
+                                        <div>·</div>
+                                        <div>{team?.conf} {team?.divison}</div>
+                                    </div>
+                                    <div className='playbye'>
+                                        <div className='playnext'>Next Week Matchup</div>
+                                        <img style={{ height: '30px', width: '30px' }} src={team?.logo_img} />
+                                        <div>Bye</div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+
+
+
+
+
+
+
+
+
                     </div>
-                    <div></div>
+
                 </div>
             }
         </div>
