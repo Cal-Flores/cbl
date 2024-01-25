a72b40d08f3f72c9c573358b34c94ff44f8c40aa
calebflores <calebflores1388@gmail.com>
2024-01-25 14:48:55 -0600

    indivudal team schedule componnent start

--- react-app/src/components/TeamSchedule/teamSchedule.js
+++ react-app/src/components/TeamSchedule/teamSchedule.js
@@ -9,10 +9,39 @@ function TeamSchedule() {
     const history = useHistory();
     const routeParams = useParams();
     const teamId = routeParams?.teamId
+    const [teamSchedule, setTeamSchedule] = useState([]);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/api/team_routes/schedule/${teamId}`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                setTeamSchedule(data.Duals);
+            } catch (error) {
+                console.error('Error fetching team schedule:', error);
+                // Handle error as needed
+            }
+        };
+
+        if (teamId) {
+            fetchData();
+        }
+    }, [teamId]);
 
     return (
         <div style={{ marginTop: '5%' }}>
             <div>TEAMS SCHEDULE</div>
+            <ul>
+                {Object.values(teamSchedule).map((dual) => (
+                    <li key={dual.id}>{/* Render dual information here */}</li>
+                ))}
+            </ul>
+
         </div>
     )
 }
