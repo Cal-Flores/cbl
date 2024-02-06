96f1de9af5640e57535958c8fb1283d7fefcc8fd
calebflores <calebflores1388@gmail.com>
2024-02-06 14:23:07 -0600

    elo score hash

--- app/api/team_routes.py
+++ app/api/team_routes.py
@@ -495,3 +495,18 @@ print(f"New winner rating: {new_winner_rating}")
 print(f"New loser rating: {new_loser_rating}")
 
 
+# /**
+#  * @param {number[]} nums
+#  * @return {boolean}
+#  */
+# var canJump = function(nums) {
+#     let max = 0;
+
+#     for(let i =0; i<nums.length;i++){
+#         max = Math.max(nums[i] + i,max);
+#         if(max >= nums.length-1) return true;
+#         if(max == i && nums[i] == 0) return false;
+#     }
+
+#     return false;
+
